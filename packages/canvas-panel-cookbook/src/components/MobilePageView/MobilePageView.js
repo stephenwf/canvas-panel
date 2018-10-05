ed211fb44b428cf9b443d687604a4d39a5a435e2
stephenwf <stephen.fraser@digirati.com>
2018-10-05 11:26:58 +0100

    Static working copy

--- packages/canvas-panel-cookbook/src/components/MobilePageView/MobilePageView.js
+++ packages/canvas-panel-cookbook/src/components/MobilePageView/MobilePageView.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import { withBemClass, StaticImageViewport } from '@canvas-panel/core';
+import {
+  withBemClass,
+  StaticImageViewport,
+  FullPageViewport,
+} from '@canvas-panel/core';
 import CanvasDetail from '../CanvasDetail/CanvasDetail';
 import './MobilePageView.scss';
 import SlideShow from '../Slideshow/SlideShow';
@@ -10,7 +14,7 @@ import posed, { PoseGroup } from 'react-pose';
 import Carousel from 'nuka-carousel';
 import FlipMove from 'react-flip-move';
 
-const PosedImage = props => <div {...props} />;
+// const PosedImage = props => <div {...props} />;
 //
 // const PosedImage = posed.div({
 //   flip: {
@@ -70,17 +74,127 @@ class FullScreenMobilePageView extends Component {
     return (
       <div
         style={{
-          transform: down
-            ? `translate(${xDelta}px, ${0}px)`
-            : `translate(0px, 0px)`,
-          transition: down ? null : 'transform .2s',
+          // transform: down
+          //   ? `translate(${xDelta}px, ${0}px)`
+          //   : `translate(0px, 0px)`,
+          // transition: down ? null : 'transform .2s',
           display: 'flex',
           flexDirection: 'row',
           alignItems: 'center',
           height: '100vh',
         }}
       >
-        {children}
+        {children(xDelta)}
+      </div>
+    );
+  }
+}
+
+@withGesture
+class PeakComponent extends Component {
+  static defaultProps = {
+    threshold: 150,
+    onNext: () => null,
+    onPrevious: () => null,
+  };
+
+  state = { down: false, revert: false };
+
+  componentWillReceiveProps(nextProps, nextContext) {
+    if (
+      nextProps.down === false &&
+      this.state.down === true &&
+      this.props.index !== nextProps.index
+    ) {
+      this.setState({ down: false, revert: false });
+    }
+
+    if (this.props.down && nextProps.down === false) {
+      // @todo add length check here to revert when you reach the end.
+      if (nextProps.xDelta >= nextProps.threshold) {
+        nextProps.onPrevious();
+      } else if (nextProps.xDelta <= -nextProps.threshold) {
+        nextProps.onNext();
+      } else {
+        this.setState({ down: false, revert: true });
+      }
+    }
+    if (this.state.down === false && nextProps.down === true) {
+      this.setState({ down: true });
+    }
+  }
+
+  render() {
+    const { down, revert } = this.state;
+    const {
+      customOffset,
+      xDelta,
+      index,
+      renderLeft,
+      renderRight,
+      children,
+    } = this.props;
+    const x = (down ? xDelta : 0) + customOffset;
+    const shouldAnimate = revert && down === false;
+
+    return (
+      <div
+        style={{
+          position: 'fixed',
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+          background: '#000',
+          color: '#fff',
+          zIndex: 9999999,
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <div style={{ flex: 1, position: 'relative', height: '100%' }}>
+          <FlipMove typeName={null} enterAnimation="none" leaveAnimation="none">
+            <div
+              key={index - 1}
+              style={{
+                position: 'absolute',
+                height: 600,
+                width: '100%',
+                top: 0,
+                left: `calc(-100% + ${x}px)`,
+                transition: shouldAnimate ? 'left .2s' : null,
+              }}
+            >
+              {renderLeft()}
+            </div>
+            <div
+              key={index}
+              style={{
+                position: 'absolute',
+                height: 600,
+                width: '100%',
+                left: `calc(0px + ${x}px)`,
+                top: 0,
+                transition: shouldAnimate ? 'left .2s' : null,
+              }}
+            >
+              {children}
+            </div>
+            <div
+              key={index + 1}
+              style={{
+                position: 'absolute',
+                height: 600,
+                width: '100%',
+                left: `calc(100% + ${x}px)`,
+                top: 0,
+                transition: shouldAnimate ? 'left .2s' : null,
+              }}
+            >
+              {renderRight()}
+            </div>
+          </FlipMove>
+        </div>
       </div>
     );
   }
@@ -88,12 +202,148 @@ class FullScreenMobilePageView extends Component {
 
 const ConnectedFSMPV = withGesture(FullScreenMobilePageView);
 
+class MobileViewer extends Component {
+  state = { open: false };
+  render() {
+    const { open } = this.state;
+    const { ...props } = this.props;
+    return (
+      <div>
+        <div onClick={() => this.setState(s => ({ open: !s.open }))}>
+          <StaticImageViewport {...props} />
+        </div>
+        {open ? <div>TESTING CONTENT</div> : null}
+      </div>
+    );
+  }
+}
+
 class MobilePageView extends Component {
-  state = { isFullscreen: true, currentCanvas: null };
+  state = { isFullscreen: true, currentCanvas: null, __mount: false };
+
+  componentWillMount() {
+    setInterval(() => {
+      this.setState(s => ({ __mount: !s.__mount }));
+    }, 1000);
+  }
 
   render() {
-    const { isFullscreen } = this.state;
+    const { isFullscreen, __mount } = this.state;
     const { bem, manifest } = this.props;
+
+    if (isFullscreen) {
+      const {
+        canvas,
+        currentIndex,
+        region,
+        fullscreenProps,
+        previousRange,
+        nextRange,
+      } = this.props;
+
+      const next = manifest
+        .getSequenceByIndex(0)
+        .getCanvasByIndex(currentIndex + 1);
+
+      const prev = manifest
+        .getSequenceByIndex(0)
+        .getCanvasByIndex(currentIndex - 1);
+
+      return (
+        <PeakComponent
+          customOffset={0}
+          onNext={nextRange}
+          onPrevious={previousRange}
+          renderLeft={() =>
+            prev ? (
+              <MobileViewer
+                manifest={manifest}
+                canvas={prev}
+                maxHeight={200}
+                maxWidth={200}
+              />
+            ) : null
+          }
+          renderRight={() =>
+            next ? (
+              <MobileViewer
+                manifest={manifest}
+                canvas={next}
+                maxHeight={200}
+                maxWidth={200}
+              />
+            ) : null
+          }
+          index={currentIndex}
+        >
+          <MobileViewer
+            manifest={manifest}
+            canvas={canvas}
+            maxHeight={200}
+            maxWidth={200}
+          />
+        </PeakComponent>
+      );
+
+      return (
+        <div
+          style={{
+            position: 'fixed',
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            background: '#000',
+            color: '#fff',
+            zIndex: 9999999,
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
+          <div style={{ flex: 1, position: 'relative' }}>
+            <div
+              style={{
+                position: 'absolute',
+                height: 600,
+                width: '100%',
+                background: 'blue',
+                opacity: 0.5,
+                transform: `translateX(calc(100% + ${
+                  __mount ? '0px' : '-100%'
+                }))`,
+                transition: 'transform .3s',
+              }}
+            />
+            <div
+              style={{
+                position: 'absolute',
+                height: 600,
+                width: '100%',
+                opacity: 0.5,
+                background: 'green',
+                transform: `translateX(calc(0% + ${
+                  __mount ? '0px' : '-100%'
+                }))`,
+                transition: 'transform .3s',
+              }}
+            />
+            <div
+              style={{
+                position: 'absolute',
+                height: 600,
+                width: '100%',
+                opacity: 0.5,
+                background: 'red',
+                transform: `translateX(calc(-100% + ${
+                  __mount ? '0px' : '-100%'
+                }))`,
+                transition: 'transform .3s',
+              }}
+            />
+          </div>
+        </div>
+      );
+    }
 
     if (isFullscreen) {
       const {
@@ -119,6 +369,7 @@ class MobilePageView extends Component {
         width: '50%',
         display: 'block',
         background: 'red',
+        transition: 'transform .3s',
       };
 
       return (
@@ -139,65 +390,82 @@ class MobilePageView extends Component {
           <CanvasDetail canvas={canvas}>
             {({ label, body, attributionLabel, attribution }) => (
               <ConnectedFSMPV onNext={nextRange} onPrevious={previousRange}>
-                <FlipMove typeName={null}>
-                  {prev ? (
-                    <PosedImage
-                      key={currentIndex - 1}
-                      data-key={currentIndex - 1}
-                      style={flexStyle}
-                    >
-                      <StaticImageViewport
-                        manifest={manifest}
-                        canvas={prev}
-                        maxHeight={200}
-                        maxWidth={200}
-                      />
-                    </PosedImage>
-                  ) : (
-                    <PosedImage
-                      key={currentIndex - 1}
-                      data-key={currentIndex - 1}
-                      style={flexStyle}
-                    />
-                  )}
-                  <PosedImage
-                    key={currentIndex}
-                    data-key={currentIndex}
-                    style={flexStyle}
-                  >
-                    <StaticImageViewport
-                      className={bem.element('canvas-image')}
-                      manifest={manifest}
-                      canvas={canvas}
-                      maxHeight={200}
-                      maxWidth={200}
-                    >
-                      <div className={bem.element('attribution')}>
-                        {attributionLabel} {attribution}
+                {xDelta => (
+                  <FlipMove typeName={null}>
+                    {prev ? (
+                      <div
+                        key={currentIndex - 1}
+                        data-key={currentIndex - 1}
+                        style={{
+                          ...flexStyle,
+                          transform: `translateX(${xDelta}px)`,
+                        }}
+                      >
+                        <StaticImageViewport
+                          manifest={manifest}
+                          canvas={prev}
+                          maxHeight={200}
+                          maxWidth={200}
+                        />
                       </div>
-                    </StaticImageViewport>
-                  </PosedImage>
-                  {next ? (
-                    <PosedImage
-                      key={currentIndex + 1}
-                      data-key={currentIndex + 1}
-                      style={flexStyle}
+                    ) : (
+                      <div
+                        key={currentIndex - 1}
+                        data-key={currentIndex - 1}
+                        style={{
+                          ...flexStyle,
+                          transform: `translateX(${xDelta}px)`,
+                        }}
+                      />
+                    )}
+                    <div
+                      key={currentIndex}
+                      data-key={currentIndex}
+                      style={{
+                        ...flexStyle,
+                        transform: `translateX(${xDelta}px)`,
+                      }}
                     >
                       <StaticImageViewport
+                        className={bem.element('canvas-image')}
                         manifest={manifest}
-                        canvas={next}
+                        canvas={canvas}
                         maxHeight={200}
                         maxWidth={200}
+                      >
+                        <div className={bem.element('attribution')}>
+                          {attributionLabel} {attribution}
+                        </div>
+                      </StaticImageViewport>
+                    </div>
+                    {next ? (
+                      <div
+                        key={currentIndex + 1}
+                        data-key={currentIndex + 1}
+                        style={{
+                          ...flexStyle,
+                          transform: `translateX(${xDelta}px)`,
+                        }}
+                      >
+                        <StaticImageViewport
+                          manifest={manifest}
+                          canvas={next}
+                          maxHeight={200}
+                          maxWidth={200}
+                        />
+                      </div>
+                    ) : (
+                      <div
+                        key={currentIndex + 1}
+                        data-key={currentIndex + 1}
+                        style={{
+                          ...flexStyle,
+                          transform: `translateX(${xDelta}px)`,
+                        }}
                       />
-                    </PosedImage>
-                  ) : (
-                    <PosedImage
-                      key={currentIndex + 1}
-                      data-key={currentIndex + 1}
-                      style={flexStyle}
-                    />
-                  )}
-                </FlipMove>
+                    )}
+                  </FlipMove>
+                )}
               </ConnectedFSMPV>
             )}
           </CanvasDetail>
